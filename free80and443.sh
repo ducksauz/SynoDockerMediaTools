d9c50f1644d88cedeaf53b685e5cb9079f705150
ducksauz <john@duksta.org>
2017-07-29 10:36:13 -0700

    Add script to free up ports 80 and 443 on synology. See http://tonylawrence.com/post/unix/synology/freeing-port-80/ for more info

---
+++ free80and443.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sed -i -e 's/80/81/' -e 's/443/444/' /usr/syno/share/nginx/server.mustache /usr/syno/share/nginx/DSM.mustache /usr/syno/share/nginx/WWWService.mustache
+synoservicecfg --restart nginx
+
